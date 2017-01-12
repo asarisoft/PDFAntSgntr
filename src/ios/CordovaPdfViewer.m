45f9977f518ea05464f6a73da5abf0dadb4bc06d
Raphael Alla <raphael@mitija.com>
2017-01-12 22:56:56 +1100

    Starting to use native library to view pdf

--- src/ios/CordovaPdfViewer.m
+++ src/ios/CordovaPdfViewer.m
@@ -9,26 +9,28 @@
 #import "CordovaPdfViewer.h"
 #import <Cordova/CDV.h>
 
+#import "ReaderViewController.h"
+
 @implementation CordovaPdfViewer
 
 - (void)show:(CDVInvokedUrlCommand*)command
 {
-    NSString* src = [command.arguments objectAtIndex:0];
-    int x = [[command.arguments objectAtIndex:1] intValue];
-    int y = [[command.arguments objectAtIndex:2] intValue];
-    int w = [[command.arguments objectAtIndex:3] intValue];
-    int h = [[command.arguments objectAtIndex:4] intValue];
+    NSString* filename = [command.arguments objectAtIndex:0];
+    NSString* directory = [command.arguments objectAtIndex:1];
+    int x = [[command.arguments objectAtIndex:2] intValue];
+    int y = [[command.arguments objectAtIndex:3] intValue];
+    int w = [[command.arguments objectAtIndex:4] intValue];
+    int h = [[command.arguments objectAtIndex:5] intValue];
 
-    NSLog(@"src=%@ x=%d y=%d h=%d w=%d", src, x, y, w, h);
+    NSLog(@"filename=%@ directory=%@ x=%d y=%d h=%d w=%d", filename, directory, x, y, w, h);
 
     NSLog(@"Trying to display using pdf reader");
 
-    CGRect myBox = CGRectMake(300, 150, 724, 618);
+    CGRect myBox = CGRectMake(x, y, w, h);
 
     NSBundle* main = [NSBundle mainBundle];
-    NSString *localPath = [main pathForResource:@"exemple" ofType:@"pdf" inDirectory:@"www"];
+    NSString *localPath = [main pathForResource: filename ofType:@"pdf" inDirectory: directory];
 
-    /*
     ReaderDocument *document = [ReaderDocument withDocumentFilePath:localPath password: nil];
     ReaderViewController *readerViewController = [[ReaderViewController alloc] initWithReaderDocument:document];
     [self.viewController addChildViewController: readerViewController];
@@ -39,7 +41,6 @@
 
     readerViewController.view.frame = myBox;
     [self.webView addSubview:readerViewController.view];
-    */
 
     CDVPluginResult* pluginResult = nil;
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
--- www/CordovaPdfViewer.js
+++ www/CordovaPdfViewer.js
@@ -1,6 +1,17 @@
 var exec = require('cordova/exec');
 
 exports.show = function(viewerId, src, success, error) {
+    var extension = src.split('.').pop();
+
+    console.log('Source ' + src);
+    console.log('extension ' + extension);
+
+    if (extension != 'pdf') {
+        msg = 'File extension must be pdf';
+        console.log(msg);
+        error(msg);
+        return;
+    }
 
     var elem = document.getElementById(viewerId);
     if (!elem) {
@@ -10,10 +21,16 @@ exports.show = function(viewerId, src, success, error) {
         return;
     }
 
+    var filename = src.replace( /.*\//, "" );
+    var directory = src.slice(src, -1 * filename.length - 1); // -1 for the /
+    //var directory = 'toto';
+    filename = filename.slice(0, -4);
+    console.log('dir=' + directory + '  filename=' + filename);
+
     var rect = elem.getBoundingClientRect();
     console.log(rect);
 
-    exec(success, error, "CordovaPdfViewer", "show", [src, rect.top, rect.left, rect.width, rect.height]);
+    exec(success, error, "CordovaPdfViewer", "show", [filename, directory, rect.top, rect.left, rect.width, rect.height]);
 };
 
 
